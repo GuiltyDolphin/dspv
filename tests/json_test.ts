35e96f76748d0fd9fc29266891a151737077b5a3
Ben Moon <software@guiltydolphin.com>
2021-05-12 13:12:02 +0100

    Refactor tests to be grouped

--- tests/json_test.ts
+++ tests/json_test.ts
@@ -13,145 +13,146 @@ import {
 
 const basicParser = new JsonParser();
 
-Deno.test("parseAsOrThrow, array, empty array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[]", Array), []);
-});
+type InnerTest = (topDesc: string) => void;
 
-Deno.test("parseAsOrThrow, array, singleton number array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[1]", Array), [1]);
-});
+interface Testable {
+    /** Run the test as a subtest of another test. */
+    runAsInner: InnerTest;
 
-Deno.test("parseAsOrThrow, array, mixed element array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[1, true, [5], \"test\"]", Array), [1, true, [5], "test"]);
-});
+    /** Run the test as a top-level test. */
+    runAsMain: () => void;
+}
 
-Deno.test("parseAsOrThrow, array, not an array (an object)", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("{}", Array), JsonParser.JsonTypeError);
-});
+class Test implements Testable {
+    description: string;
+    runner: () => void;
 
-Deno.test("parseAsOrThrow, array of arrays, singleton number nested array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[[1]]", [Array, Array]), [[1]]);
-});
+    constructor(description: string, runner: () => void) {
+        this.description = description;
+        this.runner = runner;
+    }
 
-Deno.test("parseAsOrThrow, array of booleans, empty array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[]", [Array, Boolean]), []);
-});
+    runAsInner(topDesc: string) {
+        Deno.test(`${topDesc}, ${this.description}`, this.runner);
+    }
 
-Deno.test("parseAsOrThrow, array of booleans, singleton boolean array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[true]", [Array, Boolean]), [true]);
-});
+    runAsMain() {
+        Deno.test(`${this.description}`, this.runner);
+    }
+}
 
-Deno.test("parseAsOrThrow, array of booleans, not an array (an object)", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("{}", [Array, Boolean]), JsonParser.JsonTypeError);
-});
+class TestGroup implements Testable {
+    private namePart: string;
 
-Deno.test("parseAsOrThrow, array of booleans, array of numbers", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("[1]", [Array, Boolean]), JsonParser.JsonTypeError);
-});
+    private tests: Testable[];
 
-Deno.test("parseAsOrThrow, boolean, true", () => {
-    assertEquals(basicParser.parseAsOrThrow("true", Boolean), true);
-});
+    constructor(namePart: string, ...tests: Testable[]) {
+        this.namePart = namePart;
+        this.tests = tests;
+    }
 
-Deno.test("parseAsOrThrow, boolean, false", () => {
-    assertEquals(basicParser.parseAsOrThrow("false", Boolean), false);
-});
+    runAsInner(topDesc: string) {
+        this.tests.map(t => t.runAsInner(`${topDesc}, ${this.namePart}`));
+    }
 
-Deno.test("parseAsOrThrow, boolean, not a boolean", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("null", Boolean), JsonParser.JsonTypeError);
-});
+    runAsMain() {
+        this.tests.map(t => t.runAsInner(`${this.namePart}`));
+    }
+}
 
-Deno.test("parseAsOrThrow, number, 7", () => {
-    assertEquals(basicParser.parseAsOrThrow("7", Number), 7);
-});
+function testGroup(topDesc: string, ...tests: Testable[]): TestGroup {
+    return new TestGroup(topDesc, ...tests);
+}
 
-Deno.test("parseAsOrThrow, number, not a number", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("true", Number), JsonParser.JsonTypeError);
-});
+function testParseAsOrThrowWithParser(parser: JsonParser, innerDesc: string, toParse: string, ty: any, expected: any): Test {
+    return new Test(innerDesc, () => {
+        assertEquals(parser.parseAsOrThrow(toParse, ty), expected);
+    });
+}
 
-Deno.test("parseAsOrThrow, null, null", () => {
-    assertEquals(basicParser.parseAsOrThrow("null", null), null);
-});
+function testParseAsOrThrowFailsWithParser(parser: JsonParser, innerDesc: string, toParse: string, ty: any, errTy: { new(...args: any[]): any }, msgIncludes?: string): Test {
+    //function testParseAsOrThrowFailsWithParser(parser: JsonParser, innerDesc: string, toParse: string, ty: ParseTySpec<any>, errTy: { new(...args: any[]): any }, msgIncludes?: string) {
+    return new Test(innerDesc, () => {
+        assertThrows(() => parser.parseAsOrThrow(toParse, ty), errTy, msgIncludes);
+    });
+}
 
-Deno.test("parseAsOrThrow, null, not null", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("true", null), JsonParser.JsonTypeError);
-});
+function testParseAsOrThrow(innerDesc: string, toParse: string, ty: any, expected: any): Test {
+    return testParseAsOrThrowWithParser(basicParser, innerDesc, toParse, ty, expected);
+}
 
-Deno.test("parseAsOrThrow, object, empty object", () => {
-    assertEquals(basicParser.parseAsOrThrow("{}", Object), {});
-});
+function testParseAsOrThrowFails(innerDesc: string, toParse: string, ty: any, errTy: { new(...args: any[]): any }, msgIncludes?: string): Test {
+    return testParseAsOrThrowFailsWithParser(basicParser, innerDesc, toParse, ty, errTy, msgIncludes);
+}
 
-Deno.test("parseAsOrThrow, object, singleton number object", () => {
-    assertEquals(basicParser.parseAsOrThrow(`{"k": 1}`, Object), { k: 1 });
-});
+testGroup("parseAsOrThrow",
+    testGroup("array",
+        testParseAsOrThrow("empty array", "[]", Array, []),
+        testParseAsOrThrow("singleton number array", "[1]", Array, [1]),
+        testParseAsOrThrow("mixed element array", "[1, true, [5], \"test\"]", Array, [1, true, [5], "test"]),
+        testParseAsOrThrowFails("not an array (an object)", "{}", Array, JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, object, mixed element object", () => {
-    assertEquals(basicParser.parseAsOrThrow(`{"k1": 1, "k2": true, "k3": {"k31": [7]}, "k4": \"test\"}`, Object), { k1: 1, k2: true, k3: { k31: [7] }, k4: "test" });
-});
+    testGroup("array of arrays",
+        testParseAsOrThrow("singleton number nested array", "[[1]]", [Array, Array], [[1]]),
+    ),
 
-Deno.test("parseAsOrThrow, object, not an object (an array)", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("[]", Object), JsonParser.JsonTypeError);
-});
+    testGroup("array of booleans",
+        testParseAsOrThrow("empty array", "[]", [Array, Boolean], []),
+        testParseAsOrThrow("singleton boolean array", "[true]", [Array, Boolean], [true]),
+        testParseAsOrThrowFails("not an array (an object)", "{}", [Array, Boolean], JsonParser.JsonTypeError),
+        testParseAsOrThrowFails("array of numbers", "[1]", [Array, Boolean], JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, object of objects, singleton number nested object", () => {
-    assertEquals(basicParser.parseAsOrThrow(`{"k": {"k2": 1}}`, [Object, Object]), { k: { k2: 1 } });
-});
+    testGroup("boolean",
+        testParseAsOrThrow("true", "true", Boolean, true),
+        testParseAsOrThrow("false", "false", Boolean, false),
+        testParseAsOrThrowFails("not a boolean", "null", Boolean, JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, object of booleans, empty object", () => {
-    assertEquals(basicParser.parseAsOrThrow("{}", [Object, Boolean]), {});
-});
+    testGroup("number",
+        testParseAsOrThrow("7", "7", Number, 7),
+        testParseAsOrThrowFails("not a number", "true", Number, JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, object of booleans, singleton boolean object", () => {
-    assertEquals(basicParser.parseAsOrThrow(`{"k": true}`, [Object, Boolean]), { k: true });
-});
+    testGroup("null",
+        testParseAsOrThrow("null", "null", null, null),
+        testParseAsOrThrowFails("not null", "true", null, JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, object of booleans, not an object (an array)", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("[]", [Object, Boolean]), JsonParser.JsonTypeError);
-});
+    testGroup("object",
+        testParseAsOrThrow("empty object", "{}", Object, {}),
+        testParseAsOrThrow("singleton number object", `{ "k": 1 }`, Object, { k: 1 }),
+        testParseAsOrThrow("mixed element object", `{"k1": 1, "k2": true, "k3": { "k31": [7] }, "k4": \"test\"}`, Object, { k1: 1, k2: true, k3: { k31: [7] }, k4: "test" }),
+        testParseAsOrThrowFails("not an object (an array)", "[]", Object, JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, object of booleans, object of numbers", () => {
-    assertThrows(() => basicParser.parseAsOrThrow(`{"k": 1}`, [Object, Boolean]), JsonParser.JsonTypeError);
-});
+    testGroup("object of objects",
+        testParseAsOrThrow("singleton number nested object", `{"k": {"k2": 1}}`, [Object, Object], { k: { k2: 1 } }),
+    ),
 
-Deno.test("parseAsOrThrow, string, empty string", () => {
-    assertEquals(basicParser.parseAsOrThrow("\"\"", String), "");
-});
+    testGroup("object of booleans",
+        testParseAsOrThrow("empty object", "{}", [Object, Boolean], {}),
+        testParseAsOrThrow("singleton boolean object", `{"k": true}`, [Object, Boolean], { k: true }),
+        testParseAsOrThrowFails("not an object (an array)", "[]", [Object, Boolean], JsonParser.JsonTypeError),
+        testParseAsOrThrowFails("object of numbers", `{"k": 1}`, [Object, Boolean], JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, string, nonempty string", () => {
-    assertEquals(basicParser.parseAsOrThrow("\"test\"", String), "test");
-});
+    testGroup("string",
+        testParseAsOrThrow("empty string", "\"\"", String, ""),
+        testParseAsOrThrow("nonempty string", "\"test\"", String, "test"),
+        testParseAsOrThrow("string with quotes", "\"t\\\"es\\\"t\"", String, "t\"es\"t"),
+        testParseAsOrThrow("a string", "\"test\"", String, "test"),
+        testParseAsOrThrowFails("not a string", "true", String, JsonParser.JsonTypeError),
+    ),
 
-Deno.test("parseAsOrThrow, string, string with quotes", () => {
-    assertEquals(basicParser.parseAsOrThrow("\"t\\\"es\\\"t\"", String), "t\"es\"t");
-});
-
-Deno.test("parseAsOrThrow, string, not a string", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("true", String), JsonParser.JsonTypeError);
-});
-
-Deno.test("parseAsOrThrow, string, a string", () => {
-    assertEquals(basicParser.parseAsOrThrow("\"test\"", String), "test");
-});
-
-Deno.test("parseAsOrThrow, string, not a string", () => {
-    assertThrows(() => basicParser.parseAsOrThrow("true", String), JsonParser.JsonTypeError);
-});
-
-Deno.test("parseAsOrThrow, AnyTy, empty array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[]", AnyTy), []);
-});
-
-Deno.test("parseAsOrThrow, AnyTy, singleton number array", () => {
-    assertEquals(basicParser.parseAsOrThrow("[1]", AnyTy), [1]);
-});
-
-Deno.test("parseAsOrThrow, AnyTy, number", () => {
-    assertEquals(basicParser.parseAsOrThrow("1", AnyTy), 1);
-});
-
-Deno.test("parseAsOrThrow, AnyTy, boolean", () => {
-    assertEquals(basicParser.parseAsOrThrow("true", AnyTy), true);
-});
+    testGroup("AnyTy",
+        testParseAsOrThrow("empty array", "[]", AnyTy, []),
+        testParseAsOrThrow("singleton number array", "[1]", AnyTy, [1]),
+        testParseAsOrThrow("number", "1", AnyTy, 1),
+        testParseAsOrThrow("boolean", "true", AnyTy, true),
+    )
+).runAsMain();
 
 class Basic {
     p: boolean;
@@ -173,21 +174,13 @@ basicSchemas.set(Basic, basicSchema);
 
 const parserBasic = new JsonParser(basicSchemas);
 
-Deno.test("parseAsOrThrow, with schema, Basic, ok", () => {
-    assertEquals(parserBasic.parseAsOrThrow(`{"p": true}`, Basic), new Basic(true));
-});
+testGroup("parseAsOrThrow, with schema, Basic",
+    testParseAsOrThrowWithParser(parserBasic, "ok", `{"p": true}`, Basic, new Basic(true)),
+    testParseAsOrThrowFailsWithParser(parserBasic, "missing key", `{}`, Basic, JsonParser.MissingKeysError, "missing keys: p"),
+    testParseAsOrThrowFailsWithParser(parserBasic, "extra key", `{"p": true, "q": 1}`, Basic, JsonParser.UnknownKeysError, "unknown keys: q"),
+    testParseAsOrThrowFailsWithParser(parserBasic, "Basic, not on an object", '7', Basic, JsonParser.JsonTypeError),
+).runAsMain();
 
-Deno.test("parseAsOrThrow, with schema, Basic, missing key", () => {
-    assertThrows(() => parserBasic.parseAsOrThrow(`{}`, Basic), JsonParser.MissingKeysError, "missing keys: p");
-});
-
-Deno.test("parseAsOrThrow, with schema, Basic, extra key", () => {
-    assertThrows(() => parserBasic.parseAsOrThrow(`{"p": true, "q": 1}`, Basic), JsonParser.UnknownKeysError, "unknown keys: q");
-});
-
-Deno.test("parseAsOrThrow, with schema, Basic, not on an object", () => {
-    assertThrows(() => parserBasic.parseAsOrThrow('7', Basic), JsonParser.JsonTypeError);
-});
 
 class Basic2 {
     p: Basic;
@@ -207,31 +200,11 @@ basic2SchemaMap.set(Basic2, basic2Schema);
 
 const basic2Parser = new JsonParser(basic2SchemaMap);
 
-Deno.test("parseAsOrThrow, with schema, Basic2, inner item does not match Basic, is empty", () => {
-    assertThrows(() => basic2Parser.parseAsOrThrow(`
-{
-  "p": {
-  }
-}`, Basic2), JsonParser.MissingKeysError, "missing keys: p");
-});
-
-Deno.test("parseAsOrThrow, with schema, Basic2, inner item does not match Basic, wrong type", () => {
-    assertThrows(() => basic2Parser.parseAsOrThrow(`
-{
-  "p": {
-    "p": 1
-  }
-}`, Basic2), JsonParser.JsonTypeError, "expected: boolean");
-});
-
-Deno.test("parseAsOrThrow, with schema, Basic2, ok", () => {
-    assertEquals(basic2Parser.parseAsOrThrow(`
-{
-  "p": {
-    "p": true
-  }
-}`, Basic2), new Basic2(new Basic(true)))
-});
+testGroup("parseAsOrThrow, with schema, Basic2",
+    testParseAsOrThrowFailsWithParser(basic2Parser, "inner item does not match Basic, is empty", `{"p": {}}`, Basic2, JsonParser.MissingKeysError, "missing keys: p"),
+    testParseAsOrThrowFailsWithParser(basic2Parser, "inner item does not match Basic, wrong type", `{"p": {"p": 1}}`, Basic2, JsonParser.JsonTypeError, "expected: boolean"),
+    testParseAsOrThrowWithParser(basic2Parser, "ok", `{"p": {"p": true}}`, Basic2, new Basic2(new Basic(true))),
+).runAsMain();
 
 
 //////////////////////////////
@@ -249,45 +222,35 @@ function assertParseFailsWith(actual: JsonParseResult<any>, expected: Error): vo
     assertEquals(actual.unwrapLeft().message, expected.message);
 }
 
-Deno.test("errors, type error, expected boolean but got number, correct error", () => {
-    assertParseFailsWith(basicParser.parseAs('1', Boolean), new JsonParser.JsonTypeError('boolean', 'number', 1))
-});
-
 class Empty { }
 
-Deno.test("errors, type error, expected Empty but got number, correct error", () => {
-    assertParseFailsWith(new JsonParser(new Map().set(Empty, JsonSchema.objectSchema<Empty>('Empty', {
-    }, (_) => new Empty()))).parseAs(`1`, Empty), new JsonParser.JsonTypeError('Empty', 'number', 1))
-});
+testGroup("errors",
+    testGroup("type error",
+        new Test("expected boolean but got number, correct error", () => {
+            assertParseFailsWith(basicParser.parseAs('1', Boolean), new JsonParser.JsonTypeError('boolean', 'number', 1))
+        }),
+        new Test("expected Empty but got number, correct error", () => {
+            assertParseFailsWith(new JsonParser(new Map().set(Empty, JsonSchema.objectSchema<Empty>('Empty', {
+            }, (_) => new Empty()))).parseAs(`1`, Empty), new JsonParser.JsonTypeError('Empty', 'number', 1))
+        }),
+        new Test("wrong field type, expected boolean but got number, correct error", () => {
+            assertParseFailsWith(basicParser.parseAs(`{"p": 1}`, [Object, Boolean]), new JsonParser.JsonTypeError('boolean', 'number', 1))
+        }),
+    ),
 
-Deno.test("errors, type error, wrong field type, expected boolean but got number, correct error", () => {
-    assertParseFailsWith(basicParser.parseAs(`
-{
-  "p": 1
-}`, [Object, Boolean]), new JsonParser.JsonTypeError('boolean', 'number', 1))
-});
+    new Test("missing keys", () => {
+        assertParseFailsWith(
+            new JsonParser(new Map().set(Empty, JsonSchema.objectSchema<Empty>('missing keys test', {
+                p1: Boolean,
+                p2: Number,
+                p3: null
+            }, (_) => new Empty()))).parseAs(`{"p2": 1}`, Empty), new JsonParser.MissingKeysError(['p1', 'p3']))
+    }),
 
-Deno.test("errors, missing keys", () => {
-    assertParseFailsWith(
-        new JsonParser(new Map().set(Empty, JsonSchema.objectSchema<Empty>('missing keys test', {
-            p1: Boolean,
-            p2: Number,
-            p3: null
-        }, (_) => new Empty()))).parseAs(`
-{
-  "p2": 1
-}`, Empty), new JsonParser.MissingKeysError(['p1', 'p3']))
-});
-
-Deno.test("errors, unknown keys", () => {
-    assertParseFailsWith(
-        new JsonParser(new Map().set(Empty, JsonSchema.objectSchema<Empty>('unknown keys test', {
-            p2: Number,
-        }, (_) => new Empty()))).parseAs(`
-{
-  "p1": true,
-  "p2": 1,
-  "p3": null
-}`, Empty), new JsonParser.UnknownKeysError(['p1', 'p3']))
-});
-});
+    new Test("unknown keys", () => {
+        assertParseFailsWith(
+            new JsonParser(new Map().set(Empty, JsonSchema.objectSchema<Empty>('unknown keys test', {
+                p2: Number,
+            }, (_) => new Empty()))).parseAs(`{"p1": true, "p2": 1, "p3": null}`, Empty), new JsonParser.UnknownKeysError(['p1', 'p3']))
+    }),
+).runAsMain();
